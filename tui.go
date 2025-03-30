58ae5a4f414e03c1ce06826dd9d3192287260767
Yuchen Liu <yl6448@nyu.edu>
2025-03-30 18:43:44 -0400

    add tui boilerplate

---
+++ tui.go
@@ -0,0 +1,79 @@
+package main
+
+// These imports will be used later on the tutorial. If you save the file
+// now, Go might complain they are unused, but that's fine.
+// You may also need to run `go mod tidy` to download bubbletea and its
+// dependencies.
+import (
+    "fmt"
+    "os"
+
+    tea "github.com/charmbracelet/bubbletea"
+)
+
+type model struct {
+	choices []string
+	cursor int
+	selected map[int]struct{}
+}
+
+func initalModel() model {
+	return model{
+		choices: []string{"carrorts", "potatoes", "onions"},
+		selected: make(map[int]struct{}),
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit	
+		case "up":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		case "down":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+		case "enter":
+			_, ok := m.selected[m.cursor]
+			if ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
+		}
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	s := "here's the market\n"
+	for i, choice := range m.choices {
+		cursor := " "
+		if m.cursor == i {
+			cursor = ">"
+		}
+		checked := " "
+		if _, ok := m.selected[i]; ok {
+			checked = "x"
+		}
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+	}
+	return s
+}
+
+func main() {
+	p := tea.NewProgram(initalModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Error occured: %v\n", err)
+		os.Exit(1)
+	}
+}
